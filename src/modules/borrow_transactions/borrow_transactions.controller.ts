08298c1a62dffe380de3cd2a4eab54f83d97bf40
Vipin Mohanan <vipinmohanan02@gmail.com>
2025-02-07 18:00:12 +0530

    borrow returned api

--- src/modules/borrow_transactions/borrow_transactions.controller.ts
+++ src/modules/borrow_transactions/borrow_transactions.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Query } from '@nestjs/common';
 import { BorrowTransactionsService } from './borrow_transactions.service';
 import { CreateBorrowTransactionDto } from './dto/create-borrow_transaction.dto';
 
@@ -15,8 +15,12 @@ export class BorrowTransactionsController {
   @Get('/viewBorrowBooks/:id')
   async borrowBookDetails(@Param('id') id :number){
     return this.borrowTransactionsService.borrowBookDetails(id);
-
   }
 
+  @Patch('/return')
+  async updateReturnedBook(@Query('user_id')user_id:number, @Query('book_id')book_id:number)
+  {
+    return await this.borrowTransactionsService.updateReturnedBook(user_id, book_id)
+  }
   
 }
--- src/modules/borrow_transactions/dto/update-borrow_transaction.dto.ts
+++ src/modules/borrow_transactions/dto/update-borrow_transaction.dto.ts
@@ -1,5 +1,25 @@
 /* eslint-disable prettier/prettier */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBorrowTransactionDto } from './create-borrow_transaction.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateBorrowTransactionDto extends PartialType(CreateBorrowTransactionDto) {}
+export class UpdateBorrowTransactionDto extends PartialType(CreateBorrowTransactionDto) {
+
+        @IsOptional()
+        book_id :number
+    
+        @IsOptional()
+        user_id :number
+    
+        @IsOptional()
+        borrow_date:string | null;
+    
+        @IsOptional()
+        due_date:string | null;
+    
+        @IsOptional()
+        status:string
+    
+        @IsOptional() // Allows return_date to be omitted or set as null
+        return_date?: string | null;
+}
